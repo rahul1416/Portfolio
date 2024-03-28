3490ccf71d9d4b157409d33eb94fbd503206d8ae
rahul1416 <rahuluino@gmail.com>
2024-03-28 19:24:07 +0530

    added pages

---
+++ src/data/constants.js
@@ -0,0 +1,377 @@
+export const Bio = {
+  name: "Rahul",
+  roles: [
+    "DSAI Engineering Student",
+    "Mahine Leaning Engineer",
+    "Data Analysts",
+    "Programmer",
+  ],
+  description:
+    "I am a dedicated Data Science and Artificial Intelligence engineering student with a passion for leveraging cutting-edge technologies to create impactful solutions. My primary focus lies in the realms of Data Science and Machine Learning, where I continuously strive to innovate and push the boundaries of what is possible.",
+  github: "https://github.com/rahul1416",
+  resume:
+    "https://drive.google.com/file/d/1GwFl0W-wrY1KXSEh2koyHtIvFnrW4kES/view?usp=drive_link",
+  linkedin: "https://www.linkedin.com/in/rahul-86161a249/",
+  twitter: "https://twitter.com/",
+  insta: "https://www.instagram.com/im_rahul_i/",
+  facebook: "https://www.facebook.com/profile.php?id=61551745742501",
+};
+
+export const skills = [
+  {
+    title: "Machine Learning",
+    skills: [
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "TensorFlow",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/2/2d/Tensorflow_logo.svg",
+      },
+      {
+        name: "PyTorch",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/1/10/PyTorch_logo_icon.svg",
+      },
+      {
+        name: "NumPy",
+        image: "https://upload.wikimedia.org/wikipedia/commons/3/31/NumPy_logo_2020.svg",
+      },
+      {
+        name: "Pandas",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/e/ed/Pandas_logo.svg",
+      },
+      {
+        name: "Matplotlib",
+        image:
+          "https://upload.wikimedia.org/wikipedia/en/5/56/Matplotlib_logo.svg",
+      },
+      {
+        name: "plotly",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/8/8a/Plotly-logo.png",
+      },
+      {
+        name: "scikit learn",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/0/05/Scikit_learn_logo_small.svg",
+      },
+      // {
+      //   name: "Flutter",
+      //   image:
+      //     "https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png",
+      // },
+    ],
+  },
+  {
+    title: "Natural Language Processing",
+    skills: [
+      {
+        name: "NLTK",
+        image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "spaCy",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/8/88/SpaCy_logo.svg",
+      },
+      {
+        name: "Transformers",
+        image: "https://upload.wikimedia.org/wikipedia/en/4/45/Hugging_Face_logo.svg",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "LSTM",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/5/53/Peephole_Long_Short-Term_Memory.svg",
+      },
+      {
+        name: "BERT",
+        image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+      },
+      {
+        name: "GPT",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/0/04/ChatGPT_logo.svg",
+      },
+      {
+        name: "PyTorch",
+        image: "https://upload.wikimedia.org/wikipedia/commons/1/10/PyTorch_logo_icon.svg",
+      },
+      {
+        name: "Tensorflow",
+        image: "https://upload.wikimedia.org/wikipedia/commons/2/2d/Tensorflow_logo.svg",
+      },
+      {
+        name: "selenium",
+        image: "https://upload.wikimedia.org/wikipedia/commons/9/9f/Selenium_logo.svg",
+      },
+    ],
+  },
+  {
+    title: "Frontend and Backend",
+    skills: [
+      {
+        name: "HTML",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png",
+      },
+      {
+        name: "Django",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/7/75/Django_logo.svg",
+      },
+      {
+        name: "NodeJS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/e/e0/Git-logo.svg",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+      {
+        name: "Hadoop",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/3/38/Hadoop_logo_new.svg",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Kafka",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/0/05/Apache_kafka.svg",
+      },
+      {
+        name: "Adobe PhotoShop",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/a/af/Adobe_Photoshop_CC_icon.svg",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/3/33/Figma-logo.svg",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "interiit.png",
+    role: "Inter-IIT Tech Meet",
+    company: "IIT Madras",
+    date: "Nov 2023 - dec 2023",
+    desc: "Developing a trading system capable of navigating the dynamic cryptocurrency market. Constraints include limited positions (long and short), capital allocation strategies, and buy/sell decisions based on the trading signals.",
+    skills: [
+      "Python",
+      "GNN",
+      "Time Series",
+      "PyTorch",
+      "Genetic Algorithm",
+      "TSMixer",
+      "Tensorflow",
+    ],
+    doc: "https://media.licdn.com/dms/image/D4D2DAQFlp60ZqHuaFQ/profile-treasury-image-shrink_1280_1280/0/1691180828512?e=1692381600&v=beta&t=mM5Y_NE5EPlQhez5FAN6NLVSKcO_Ojt_9Gq3mnFGkAQ",
+  },
+  {
+    id: 1,
+    img: "CW.png",
+    role: " AI/ML Intern",
+    company: "CompWallah",
+    date: "March 2023 - present",
+    desc: "Working on AI/ML Projects like delivary time predictions and credit score classifications.",
+    skills: [
+      "Python",
+      "NumPy",
+      "Scikit-learn",
+      "Matplotlib",
+      "Seaborn",
+      "PyTorch",
+      "Tensorflow",
+    ],
+    doc: "https://media.licdn.com/dms/image/D4D2DAQFlp60ZqHuaFQ/profile-treasury-image-shrink_1280_1280/0/1691180828512?e=1692381600&v=beta&t=mM5Y_NE5EPlQhez5FAN6NLVSKcO_Ojt_9Gq3mnFGkAQ",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "IIT_Bhilai_logo.png",
+    school: "Indian Institute of Technology Bhilai",
+    date: "NOV 2021 - July 2025",
+    // grade: "8.71 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Data Science and Artificial Intelligence at Indian Institute of Technology Bhilai. I have taken courses in Architecture for Management of Large Datasets, Artificial Intelligence, Digital Speech Processing, Data Analytics and Visualization, Computer Organization and Architecture , Operating Systems ,Data Structures and Algorithms, and Computer Networks among others.",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: "KV.png",
+    school: "Kendriya Vidyalaya Sector 12 Dwarka",
+    date: "Apr 2021",
+    grade: "89.33%",
+    desc: "I completed my class 12 high school education at Kendriya Vidyalaya Sector 12 dwarka, Dankuni, where I studied Science with Computer Science.",
+    degree: "CBSE(XII), Science with Computer",
+  },
+  // {
+  //   id: 2,
+  //   img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/methodist.png?alt=media&token=018a1b18-e2dd-4f34-8855-cff1b6b69fd3",
+  //   school: "Methodist School, Dankuni",
+  //   date: "Apr 2017 - Apr 2019",
+  //   grade: "92.3%",
+  //   desc: "I completed my class 10 education at Methodist School, Dankuni, where I studied Science with Computer Application.",
+  //   degree: "ICSC(X), Science with Computer",
+  // },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "BankingChatbot",
+    date: "Jan 2024 - Feb 2024",
+    description:
+      "Spearheaded the development of a Django-based Banking Chatbot backend, integrating speech recognition, text processing, and text-to-speech capabilities for efficient customer assistance. Key technologies employed in this project include Python, Django, Vosk for speech recognition, and Piper for text-to-speech conversion",
+    image:
+      "Bankingchatbot.png",
+    tags: [
+      "Vosk",
+      "Ollama",
+      "Piper",
+      "Django",
+      "Python",
+      "HTML",
+      "CSS",
+    ],
+    category: "web app",
+    github: "https://github.com/rahul1416/BankingChatBot",
+    webapp: "https://github.com/rahul1416/BankingChatBot",
+  },
+  {
+    id: 0,
+    title: "RL Spec Ops",
+    date: "Oct 2023 - Nov 2023",
+    description:
+      "Developed an innovative RL Spec Ops  Game that simulates specialized military missions, focusing on swift terrorist elimination and civilian rescue. This involved programming initial terrorist behaviors and continuously training a team of agents representing the military, aiming to enhance adaptability",
+    image:
+      "RL_spec_ops.png",
+    tags: ["TensorFlow", "OpenAI Gym", "Ray Rllib", "Python", "Pygame"],
+    category: "machine learning",
+    github: "https://github.com/Bolt17803/RL_Spec_Ops",
+    webapp: "https://github.com/rahul1416/RL_spec_ops",
+
+  },
+  {
+    id: 1,
+    title: "Temporary Dietary Patterns for Adults",
+    date: "Oct 2022 - Dec 2022",
+    description:
+      "Analyzing research papers on temporary dietary patterns in adults, I employed statistical methods to identify significant correlations and provide evidence-based recommendations for improving dietary habits and overall health outcomes.",
+    image:
+      "TDP.png",
+    tags: [
+      "Python",
+      "NumPy",
+      "Scikit-learn",
+      "Matplotlib",
+      "Seaborn",
+    ],
+    category: "machine learning",
+    github: "https://github.com/rahul1416/TDP-for-adults",
+    webapp: "https://www.overleaf.com/project/638b720d14e025765b4c8fab/invite/token/5ad638327d65ff00cd1122953c370ec0323c8a4efd4b6f87?project_name=T2_Project_Report_DS201&user_first_name=dr.nitin.khanna",
+  },
+  {
+    id: 2,
+    title: "Indeed Job Analysis",
+    date: "Jan 2023 - Mar 2023",
+    description:
+      "The project aims to analyze job vacancies in the data science field across various roles including Data Engineer, Data Analyst, Data Scientist, and Machine Learning Engineer.",
+    image:
+      "indeed.png",
+    tags: ["Python", "Selenium", "Matplotlib","Plotly", "seaborn"],
+    category: "machine learning",
+    github: "https://github.com/rahul1416/indeed_job_analysis",
+    webapp: "https://github.com/rahul1416/indeed_job_analysis",
+  },
+  // {
+  //   id: 3,
+  //   title: "Buckoid",
+  //   date: "Dec 2021 - Apr 2022",
+  //   description:
+  //     "App Is Currently In Playstore 100+ Downloads. This Project proposes an “Expense Tracking App”. Keep track of your personal expenses and compare them to your monthly income with the budget planner. It has Google Drive Cloud API for Backup of User Room Database. Made with Kotlin in MVVM Architecture & Live Data.",
+  //   image:
+  //     "https://camo.githubusercontent.com/fbf405040ae86b5d4a40f24e4ac034982cb8c0e2d850560ba64527997b258be8/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f6c6f67696e2d65613565322e61707073706f742e636f6d2f6f2f4255434b4f49442532302831292e706e673f616c743d6d6564696126746f6b656e3d32653735376235372d323964372d346263612d613562322d653164346538313432373435",
+  //   tags: ["Kotlin", "MVVM", "Room Database", "Google Drive Cloud API"],
+  //   category: "android app",
+  //   github: "https://github.com/rishavchanda/Buckoid-Android-App",
+  //   webapp: "https://play.google.com/store/apps/details?id=com.rishav.buckoid",
+  // },
+  {
+    id: 10,
+    title: "Speeker Recognition",
+    date: "Jan 2024 - Feb 2024",
+    description:
+      "Delve into the realm of speaker recognition, leveraging the effectiveness of Mel-Frequency Cepstral Coefficients (MFCC) for extracting relevant features from speech signals. Speaker recognition serves as a vital component of biometric identification systems, enabling the identification or verification of individuals based on their unique vocal characteristics.",
+    image:
+      "Speaker.png",
+    tags: ["PyAudio", "PyTorch", "Python"],
+    category: "machine learning",
+    github: "https://github.com/rahul1416/AI-ML_Projects/tree/main/Speeker_recognition",
+    webapp: "https://github.com/rahul1416/AI-ML_Projects/tree/main/Speeker_recognition",
+  },
+  {
+    id: 4,
+    title: "MNIST Digit Recognition",
+    date: "Jan 2024",
+    description:
+      " Implemented a digit recognition system using the MNIST dataset, a benchmark dataset widely used in the field of machine learning. Our objective was to develop a robust model capable of accurately recognizing handwritten digits from images.",
+    image:
+      "digit.png",
+    tags: ["PyTorch", "Streamlit", "Python", "torchvision"],
+    category: "web app",
+    github: "https://github.com/rahul1416/AI-ML_Projects/tree/main/Mnist_digit_Recognition",
+    webapp: "https://github.com/rahul1416/AI-ML_Projects/tree/main/Mnist_digit_Recognition",
+  },
+];
+
+export const TimeLineData = [
+  { year: 2021, text: "Qualified in Jee Main and Advance" },
+  { year: 2022, text: "Started journey in the field of Data Science" },
+];
